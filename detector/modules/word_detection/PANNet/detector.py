a95cb209bbf11eca7ac978c5f488bb1f4aa22c19
phungpx <phamxuanphungbku@gmail.com>
2021-11-01 17:57:51 +0700

    Update post processing for PANNet

--- detector/modules/word_detection/PANNet/detector.py
+++ detector/modules/word_detection/PANNet/detector.py
@@ -4,12 +4,13 @@ import cv2
 import numpy as np
 import torch
 from torch import nn
+import pyclipper
 
 import utils
 from abstract.processor import Processor
 
 from ..dto import Image, Point, Word
-from .postprocess import decode
+# from .postprocess import decode
 
 
 def chunks(lst: list, size: Optional[int] = None) -> Union[List, Generator]:
@@ -72,8 +73,37 @@ class WordDetector(Processor):
         for image, pred in zip(images, preds):
             if image.image is not None:
                 height, width = image.image.shape[:2]
-                pred, boxes = decode(pred)
+                pred, boxes = self.decode_clip(pred)
                 scale = pred.shape[1] / width, pred.shape[0] / height
                 image.words = [Word(box=[Point(x=point[0], y=point[1]) for point in box]) for box in boxes / scale]
 
         return images,
+
+    def decode_clip(self, preds, scale=1, threshold=0.7311, min_area=5):
+        preds[:2, :, :] = torch.sigmoid(preds[:2, :, :])
+        preds = preds.detach().cpu().numpy()
+        text = preds[0] > threshold  # text
+        kernel = (preds[1] > threshold) * text  # kernel
+
+        label_num, label = cv2.connectedComponents(kernel.astype(np.uint8), connectivity=4)
+        bbox_list = []
+        for label_idx in range(1, label_num):
+            points = np.array(np.where(label == label_idx)).transpose((1, 0))[:, ::-1]
+            if points.shape[0] < min_area:
+                continue
+            rect = cv2.minAreaRect(points)
+            poly = cv2.boxPoints(rect).astype(int)
+
+            d_i = cv2.contourArea(poly) * 1.5 / cv2.arcLength(poly, True)
+            pco = pyclipper.PyclipperOffset()
+            pco.AddPath(poly, pyclipper.JT_ROUND, pyclipper.ET_CLOSEDPOLYGON)
+            shrinked_poly = np.array(pco.Execute(d_i))
+            if shrinked_poly.size == 0:
+                continue
+            rect = cv2.minAreaRect(shrinked_poly)
+            shrinked_poly = cv2.boxPoints(rect).astype(int)
+            if cv2.contourArea(shrinked_poly) < 800 / (scale * scale):
+                continue
+
+            bbox_list.append([shrinked_poly[1], shrinked_poly[2], shrinked_poly[3], shrinked_poly[0]])
+        return label, np.array(bbox_list)
