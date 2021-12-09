cfb9a6f8c6ff72f106afec095a90f9e96251c272
phungpx <phamxuanphungbku@gmail.com>
2021-12-09 16:25:44 +0700

    Update Pan Loss

--- PANNet/flame/core/loss/l_tex_ker.py
+++ PANNet/flame/core/loss/l_tex_ker.py
@@ -1,17 +1,18 @@
 import torch
+import warnings
 from torch import nn
 
 
 class TextKernelLoss(nn.Module):
-    def __init__(self, ohem_ratio: float = 3, epsilon: float = 1e-6):
+    def __init__(self, ohem_ratio: float = 3, smooth: float = 1e-6):
         super(TextKernelLoss, self).__init__()
-        self.epsilon = epsilon
+        self.smooth = smooth
         self.ohem_ratio = ohem_ratio
 
     def forward(self, preds: torch.Tensor, targets: torch.Tensor, effective_maps: torch.Tensor) -> torch.Tensor:
         '''
         Args:
-            preds: Tensor, N x 6 x H x W
+            preds (Tensor): The output tensor of size :math:`(N, 6, H, W)`.
             targets: Tensor, N x 2 x H x W
             effective_maps: Tensor, N x H x W
         Returns:
@@ -28,12 +29,12 @@ class TextKernelLoss(nn.Module):
         text_loss = self.dice_loss(pred_texts, target_texts, selected_masks)
 
         # kernel loss
-        selected_masks = ((pred_texts > 0.5) & (effective_maps > 0.5)).float().to(preds.device)
+        selected_masks = ((pred_texts > 0.5).float() * (effective_maps > 0.5).float()).to(preds.device)
         kernel_loss = self.dice_loss(pred_kernels, target_kernels, selected_masks)
 
         return text_loss, kernel_loss
 
-    def dice_loss(self, pred, target, effective_map):
+    def dice_loss(self, pred: torch.Tensor, target: torch.Tensor, effective_map: torch.Tensor) -> float:
         target[target > 0.5] = 1
         target[target <= 0.5] = 0
 
@@ -44,36 +45,73 @@ class TextKernelLoss(nn.Module):
         pred, target = pred * effective_map, target * effective_map
 
         PG = torch.sum(pred * target, dim=1)
-        P2 = torch.sum(pred ** 2, dim=1)
-        G2 = torch.sum(target ** 2, dim=1)
+        P2 = torch.sum(pred * pred, dim=1)
+        G2 = torch.sum(target * target, dim=1)
 
-        dice = (2 * PG) / (P2 + G2 + self.epsilon)
+        dice = (2 * PG + self.smooth) / (P2 + G2 + self.smooth)
 
         return 1 - dice
 
-    def ohem(self, pred_text, target_text, effective_map):
-        positive_number = torch.sum(target_text > 0.5) - torch.sum((target_text > 0.5) & (effective_map <= 0.5))
-        if positive_number.item() == 0:
-            return effective_map.float()
+    def ohem_image(
+        self, pred_text: torch.Tensor, target_text: torch.Tensor, effective_map: torch.Tensor
+    ) -> torch.Tensor:
+        """Sample the top-k maximal negative samples and all positive samples.
+        Args:
+            pred_text (Float32 Tensor): The pred text score of size :math:`(H, W)`.
+            target_text (Float32 Tensor): The ground truth text mask of size :math:`(H, W)`.
+            effective_map (Uint8 Tensor): The effective region mask of size :math:`(H, W)`.
+        Returns:
+            selected_mask (Bool Tensor): The sampled pixel mask of size :math:`(H, W)`.
+        """
+        assert isinstance(pred_text, torch.Tensor)
+        assert isinstance(target_text, torch.Tensor)
+        assert isinstance(effective_map, torch.Tensor)
+        assert pred_text.shape == target_text.shape
+        assert target_text.shape == effective_map.shape
+        assert len(pred_text.shape) == 2
 
-        negative_number = torch.sum(target_text <= 0.5)
-        negative_number = torch.min(positive_number * self.ohem_ratio, negative_number)
-        if negative_number.item() == 0:
-            return effective_map.float()
+        pos_num = int(
+            torch.sum(target_text > 0.5).item() - torch.sum((target_text > 0.5) * (effective_map <= 0.5)).item()
+        )
+        neg_num = int(torch.sum(target_text <= 0.5).item())
+        neg_num = min(pos_num * self.ohem_ratio, neg_num)
 
-        negative_scores = pred_text[target_text <= 0.5]
-        negative_scores = torch.sort(negative_scores, descending=True)[0]
-        score_threshold = negative_scores[negative_number - 1]
+        if (pos_num == 0) or (neg_num == 0):
+            warnings.warn('pos_num = 0 or neg_num = 0')
+            return effective_map.bool()
 
-        selected_mask = ((pred_text >= score_threshold) | (target_text > 0.5)) & (effective_map > 0.5)
+        neg_score = pred_text[target_text <= 0.5]
+        neg_score_sorted = torch.sort(neg_score, descending=True)[0]
+        threshold = neg_score_sorted[neg_num - 1]
 
-        return selected_mask.float()
+        selected_mask = (((pred_text >= threshold) + (target_text > 0.5)) > 0) * (effective_map > 0.5)
 
-    def ohem_batch(self, pred_texts, target_texts, effective_maps):
+        return selected_mask
+
+    def ohem_batch(
+        self, pred_texts: torch.Tensor, target_texts: torch.Tensor, effective_maps: torch.Tensor
+    ) -> torch.Tensor:
+        """OHEM sampling for a batch of imgs.
+        Args:
+            pred_texts (Tensor): The text scores of size :math:`(H, W)`.
+            target_texts (Tensor): The gt text masks of size :math:`(H, W)`.
+            effective_maps (Tensor): The gt effective mask of size :math:`(H, W)`.
+        Returns:
+            Tensor: The sampled mask of size :math:`(H, W)`.
+        """
+        assert isinstance(pred_texts, torch.Tensor)
+        assert isinstance(target_texts, torch.Tensor)
+        assert isinstance(effective_maps, torch.Tensor)
+        assert pred_texts.shape == target_texts.shape
+        assert target_texts.shape == effective_maps.shape
+        assert len(pred_texts.shape) == 3
+
         selected_masks = []
         for i in range(pred_texts.shape[0]):
             selected_masks.append(
-                self.ohem(pred_texts[i, :, :], target_texts[i, :, :], effective_maps[i, :, :])
+                self.ohem_image(
+                    pred_texts[i], target_texts[i], effective_maps[i]
+                )
             )
 
         selected_masks = torch.stack(selected_masks, dim=0)
--- PANNet/flame/handlers/metrics/losses/PAN_loss/l_agg_dis.py
+++ PANNet/flame/handlers/metrics/losses/PAN_loss/l_agg_dis.py
@@ -1,11 +1,13 @@
 import torch
+import itertools
 from torch import nn
 
 
-class AggregationLoss(nn.Module):
-    def __init__(self, delta_agg: float = 0.5):
-        super(AggregationLoss, self).__init__()
+class AggregationDiscriminationLoss(nn.Module):
+    def __init__(self, delta_agg: float = 0.5, delta_dis: float = 3):
+        super(AggregationDiscriminationLoss, self).__init__()
         self.delta_agg = delta_agg  # is a constant, which is set to 0.5 experimentally and used to filter easy samples.
+        self.delta_dis = delta_dis  # is a constant, which is set to 3. experimentally.
 
     def forward(self, preds: torch.Tensor, targets: torch.Tensor) -> torch.Tensor:
         '''
@@ -26,7 +28,7 @@ class AggregationLoss(nn.Module):
         target_kernels = targets[:, 1, :, :]
         target_kernels = target_kernels.reshape(num_samples, -1)  # N x (H * W)
 
-        agg_losses = []
+        agg_losses, dis_losses = [], []
         for id_sample in range(num_samples):
             similarity_vector = similarity_vectors[id_sample]
             target_text = target_texts[id_sample]
@@ -36,7 +38,9 @@ class AggregationLoss(nn.Module):
             num_texts = int(target_text.max().item() + 1)   # including background
 
             instance_agg_losses = []
+            G_Ks = []
 
+            # for each text instance (excluding background)
             for id_text in range(1, num_texts):  # all text instances excluding background
                 # get ith kernel Ki and ith text instance Ti of ground truth
                 Ti = target_text == id_text
@@ -47,13 +51,14 @@ class AggregationLoss(nn.Module):
                     continue
 
                 # get the similarty vector of the ith kernel Ki -> G(Ki) = (sum q of Ki) (F(q)) / |Ki|
-                G_Ki = similarity_vector[:, Ki].mean(dim=1, keepdim=True)  # 4 x 1
+                G_Ki = similarity_vector[:, Ki].mean(dim=1)  # 4
+                G_Ks.append(G_Ki)  # save for calculation discrimination loss
 
                 # get the similarity vector of all predicted pixels F(p) in text instance Ti
                 F_p = similarity_vector[:, Ti]  # 4 x the nums of text pixel
 
                 # compute distance: (||F(p) - G(K_i)|| - delta_agg)
-                d = torch.linalg.norm((F_p - G_Ki), ord=2, dim=0) - self.delta_agg  # the nums of text pixel
+                d = torch.linalg.norm((F_p - G_Ki.reshape(4, 1)), ord=2, dim=0) - self.delta_agg  # the nums of text pixel
 
                 # compute the distance between text pixel p and the kernel Ki of text instance Ti, D(p, K_i)
                 D_p_Ki = torch.maximum(d, torch.zeros_like(d)).pow(2)  # the nums of text pixel
@@ -68,7 +73,22 @@ class AggregationLoss(nn.Module):
                 instance_agg_loss = torch.tensor(0, device=preds.device, dtype=torch.float)
             else:
                 instance_agg_loss = torch.stack(instance_agg_losses).mean()
-
             agg_losses.append(instance_agg_loss)
 
-        return torch.stack(agg_losses)
+            # discrimination loss
+            dis_loss = 0
+            for G_Ki, G_Kj in itertools.combinations(G_Ks, 2):
+                # compute D(Ki, Kj) = max(delta_dis - ||G(K_i) - G(K_j)||, 0) ** 2
+                D_Ki_Kj = self.delta_dis - torch.linalg.norm((G_Ki - G_Kj), ord=2)  # 1
+                D_Ki_Kj = torch.maximum(D_Ki_Kj, torch.zeros_like(D_Ki_Kj)).pow(2)  # 1
+                # sum_N(sum_N(ln(D(Ki, Kj)) + 1))
+                dis_loss += torch.log(D_Ki_Kj + 1)
+
+            if len(G_Ks) > 1:
+                N = len(G_Ks)
+                dis_loss = dis_loss / (N * (N - 1))
+            else:
+                dis_loss = torch.tensor(0, device=preds.device, dtype=torch.float)
+            dis_losses.append(dis_loss)
+
+        return torch.stack(agg_losses), torch.stack(dis_losses)
